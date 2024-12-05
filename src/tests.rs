ba87ba60b348933b3d85bd3030ead35bf63c3d91
Peter Salomonsen <petersalomonsen@runbox.com>
2024-12-05 22:28:19 +0000

    test that proposal ids are continuous
    that no ids are missing

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,8 @@
+use crate::{
+    db::db_types::ProposalWithLatestSnapshotView, rpc_service, types::PaginatedResponse, Env,
+};
+use near_sdk::AccountId;
+
 /**
  * Test Nearblocks mocked transactions
  */
@@ -21,4 +26,37 @@ fn test_index() {
     // Dispatch a request to 'GET /' and validate the response.
     let response = client.get("/").dispatch();
     assert_eq!(response.into_string().unwrap(), "Welcome from fly.io!!!!!");
+}
+
+#[test]
+fn test_proposal_ids_are_continuous() {
+    use rocket::local::blocking::Client;
+
+    let client = Client::tracked(super::rocket()).expect("valid `Rocket`");
+
+    for _ in 0..50 {
+        let response = client
+            .get("/proposals?order=id_asc&limit=50&offset=0}`")
+            .dispatch();
+        let result = response
+            .into_json::<PaginatedResponse<ProposalWithLatestSnapshotView>>()
+            .unwrap();
+        if result.records.len() >= 50 {
+            break;
+        }
+    }
+    let response = client
+        .get("/proposals?order=id_asc&limit=50&offset=0}`")
+        .dispatch();
+    let result = response
+        .into_json::<PaginatedResponse<ProposalWithLatestSnapshotView>>()
+        .unwrap();
+    assert_eq!(result.records.len(), 50);
+    let env: Env = envy::from_env::<Env>().expect("Failed to load environment variables");
+    let account_id: AccountId = env.contract.parse().unwrap();
+
+    for ndx in 0..50 {
+        let proposal_id: i32 = ndx as i32;
+        assert_eq!(result.records[ndx].proposal_id, proposal_id);
+    }
 }
